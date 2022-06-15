7fcabb216054f47fee679b972f1bea64fd240aa9
Jack Neilan <102314039+NuvaGit@users.noreply.github.com>
2022-06-15 20:21:01 +0100

    Full code

---
+++ GUI.java
@@ -0,0 +1,46 @@
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener {
+	int count = 0;
+	JLabel label = new JLabel("number of clicks:  0");
+	public GUI() {
+		JFrame frame = new JFrame();
+		JButton button = new JButton("CLICK ME");
+		button.addActionListener(this);
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
+		frame.add(panel, BorderLayout.CENTER);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("OUR GUI");
+		frame.pack();
+		frame.setVisible(true); 
+		
+	}
+public static void main(String[] args) {
+
+	new GUI();
+	
+	
+	
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	count ++;
+	label.setText("number of clicks "+ count);
+	
+}
+}
